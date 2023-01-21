1ff047e61927a29433c8b799d26bec16b8978926
calrosdsa <carlosduram71@gmail.com>
2023-01-20 21:16:18 -0400

    changes2

--- main.go
+++ main.go
@@ -38,6 +38,7 @@ func main() {
 	}))
 	e.GET("webhook/",func(c echo.Context)(err error){
 		// hub := c.QueryParam("hub.mode")
+		fmt.Println("Received")
 		challenge := c.QueryParam("hub.challenge")
 		// token := c.QueryParam("hub.verification_token")
 		fmt.Println(challenge)
